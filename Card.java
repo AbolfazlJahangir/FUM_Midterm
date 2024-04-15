a0db1bb44538090d748595f99ccf401c16b83bca
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-04-15 17:42:00 +0330

    Player and Card classes, just created.

---
+++ Card.java
@@ -0,0 +1,40 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Card extends JLabel implements MouseListener {
+
+    final static int COUNT_COIN_PRICE = 5;
+
+    ImageIcon cardIcon;
+    int specialCoin;
+    String specialCoinColor;
+    String[] price = new String[COUNT_COIN_PRICE];
+    
+    Card() {
+
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) { }
+
+
+    @Override
+    public void mouseReleased(MouseEvent e) { }
+
+
+    @Override
+    public void mouseEntered(MouseEvent e) { }
+
+
+    @Override
+    public void mouseExited(MouseEvent e) { }
+
+
+}
---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+    int codePlayer;
+    String name;
+    int blackCount = 0, whiteCount = 0, blueCount = 0, redCount = 0, greenCount = 0, goldCount = 0;
+    int score = 0;
+    boolean turn;
+    
+    Player(int codePlayer) {
+        this.codePlayer = codePlayer;
+        
+    }
+}
