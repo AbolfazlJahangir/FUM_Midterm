e65abbfd59129e61930a97e0b8d7edbd2fa9e315
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2024-04-23 05:36:24 +0330

    Special coins.

--- Player.java
+++ Player.java
@@ -3,6 +3,7 @@ public class Player {
     int codePlayer;
     String name;
     int blackCount = 0, whiteCount = 0, blueCount = 0, redCount = 0, greenCount = 0, goldCount = 0;
+    int specialBlack = 0, specialWhite = 0, specialBlue = 0, specialRed = 0, specialGreen = 0;
     int score = 0;
     boolean turn;
     
